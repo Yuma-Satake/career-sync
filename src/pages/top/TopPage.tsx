444e9cd1ffa8aa0c54557baf90a8b7eed270d032
Yuma Satake <satous.y@icloud.com>
2024-06-12 11:57:34 +0900

    feat: TopPageにnavを追加

--- src/pages/top/TopPage.tsx
+++ src/pages/top/TopPage.tsx
@@ -1,5 +1,49 @@
+import { Button } from '@mui/material';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const TopPage: FC = () => {
-  return <div>TopPage</div>;
+  const router = useNavigate();
+
+  const navigate = [
+    {
+      path: '/',
+      text: 'TopPage',
+    },
+    {
+      path: '/auth',
+      text: 'AuthPage',
+    },
+    {
+      path: '/generate-plans',
+      text: 'GeneratePlansPage',
+    },
+    {
+      path: '/plans',
+      text: 'PlansPage',
+    },
+    {
+      path: '/history',
+      text: 'HistoryPage',
+    },
+  ];
+
+  return (
+    <div>
+      TopPage
+      {navigate.map((nav) => {
+        return (
+          <Button
+            key={nav.path}
+            variant="contained"
+            onClick={() => {
+              router(nav.path);
+            }}
+          >
+            {nav.text}
+          </Button>
+        );
+      })}
+    </div>
+  );
 };
