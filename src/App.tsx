ae8b9be7892c91070bb56776abb40bf2afc327cb
Yuma Satake <satous.y@icloud.com>
2024-06-12 12:25:16 +0900

    feat: themeColor追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { ThemeProvider, createTheme } from '@mui/material';
 import { RouterProvider } from './routers/RouterProvider';
 
 function App() {
-  return <RouterProvider />;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#FFA94D',
+        main: '#FE7405',
+        dark: '#D34F00',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider />
+    </ThemeProvider>
+  );
 }
 
 export default App;
