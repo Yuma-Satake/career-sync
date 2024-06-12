4fa4a54d127776ae4904d042a1f24b613a6c077d
Yuma Satake <satous.y@icloud.com>
2024-06-12 12:14:34 +0900

    feat: add TopPageButton

--- src/pages/top/TopPage.tsx
+++ src/pages/top/TopPage.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 export const TopPage: FC = () => {
   const router = useNavigate();
@@ -29,8 +30,16 @@ export const TopPage: FC = () => {
   ];
 
   return (
-    <div>
-      TopPage
+    <Container
+      maxWidth="sm"
+      sx={{
+        width: '100%',
+        height: '100dvh',
+        bgcolor: 'primary.main',
+        padding: '20px',
+      }}
+    >
+      {/* TopPage
       {navigate.map((nav) => {
         return (
           <Button
@@ -43,7 +52,49 @@ export const TopPage: FC = () => {
             {nav.text}
           </Button>
         );
-      })}
-    </div>
+      })} */}
+      {/* こっから始めるぜ */}
+      <Stack direction="row" spacing={3}>
+        <Stack
+          justifyContent="space-between"
+          sx={{
+            width: '100%',
+            height: 'auto',
+            aspectRatio: '1 / 1',
+            bgcolor: 'white',
+            borderRadius: '10px',
+            padding: '20px',
+          }}
+        >
+          <Box>
+            <Typography>日曜日</Typography>
+            <Typography variant="h3">14</Typography>
+          </Box>
+          <Button
+            variant="contained"
+            endIcon={<ExitToAppIcon />}
+            onClick={() => {
+              router('/generate-plans');
+            }}
+          >
+            候補生成
+          </Button>
+        </Stack>
+        <Stack
+          sx={{
+            width: '100%',
+            height: 'auto',
+            aspectRatio: '1 / 1',
+            bgcolor: 'white',
+          }}
+        >
+          <Typography>日曜日</Typography>
+          <Typography variant="h3">14</Typography>
+          <Button variant="contained" endIcon={<ExitToAppIcon />}>
+            候補生成
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
