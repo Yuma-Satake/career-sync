ee4d603124477576a50999d85619aeca5a02fdb8
insqneboi <shutaaa@gmail.com>
2024-06-19 11:23:52 +0900

    feat: ログイン

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import { Box, Button, Container, IconButton, Stack, Typography } from '@mui/material';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import GoogleIcon from '@mui/icons-material/Google';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 
 export const AuthPage: FC = () => {
   const router = useNavigate();
@@ -13,10 +13,44 @@ export const AuthPage: FC = () => {
         width: '100%',
         height: '100dvh',
         bgcolor: 'primary.main',
-        padding: '70px',
       }}
     >
-      <Stack direction="row" spacing={3}>
+      <IconButton
+        onClick={() => {
+          router('/');
+        }}
+        sx={{
+          top: '50px',
+          left: '10px',
+        }}
+      >
+        <Box
+          sx={{
+            bgcolor: 'gray',
+            height: '100%',
+            width: '100%',
+            position: 'absolute',
+            zIndex: 1,
+          }}
+        />
+        <ArrowCircleLeftIcon
+          sx={{
+            width: '50px',
+            height: '50px',
+            position: 'absolute',
+            color: 'white',
+            zIndex: 2,
+          }}
+        />
+      </IconButton>
+      <Stack
+        direction="row"
+        spacing={3}
+        sx={{
+          marginTop: '100px',
+          height: '70%',
+        }}
+      >
         <Stack
           justifyContent="center"
           spacing={5}
@@ -36,13 +70,13 @@ export const AuthPage: FC = () => {
                 fontFamily: 'arial',
                 fontSize: '50px',
                 fontWeight: 'bold',
-                // padding: '50px',
               }}
             >
               Login
             </Typography>
           </Box>
           <Button
+            size="small"
             startIcon={<img src="google.png" />}
             variant="contained"
             onClick={() => {
@@ -52,7 +86,6 @@ export const AuthPage: FC = () => {
               color: 'black',
               bgcolor: 'white',
               border: '1px solid black',
-              width: '500px',
               height: '70px',
               fontWeight: 'bold',
             }}
